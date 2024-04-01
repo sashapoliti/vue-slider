ac72d8b2bf7a9964d86921d416babeffa213e876
sashapoliti <sas.politi@gmail.com>
2024-04-02 00:52:29 +0200

    add Vue in slider

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
 import { slides } from "./data.js";
 
+const { createApp } = Vue;
+
+createApp({
+    data() {
+      return {
+        slides,
+        activeIndexSlide: 0,
+      };
+    },
+    methods: {
+    },
+    mounted() {
+    }
+  }).mount("#app");
